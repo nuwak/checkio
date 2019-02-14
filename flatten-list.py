e6953f1c2e4a54f825e61413b5885f6606dfb982
Igor Bozhennikov <mr.igorr@gmail.com>
2019-02-14 23:19:54 +0300

    stuff

---
+++ flatten-list.py
@@ -0,0 +1,18 @@
+def flat_list1(array):
+    res = []
+
+    for i in array:
+        res.append(i) if not isinstance(i, list) else res.extend(flat_list(i))
+
+    return res
+
+
+flat_list = f = lambda d: [d] if int == type(d) else sum(map(f, d), [])
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
